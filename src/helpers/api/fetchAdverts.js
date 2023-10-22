ad24180a027c4dc5cc3b51d793e330bc1402fedd
Maksym Ponomarenko <ponomarenko2983@gmail.com>
2023-10-22 22:50:17 +0200

    6

--- src/helpers/api/fetchAdverts.js
+++ src/helpers/api/fetchAdverts.js
@@ -3,10 +3,7 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://652fcda36c756603295db0b7.mockapi.io/api/';
 
 export const fetchAdverts = async params => {
-  try {
-      const queryParams = new URLSearchParams(params);
+  const queryParams = new URLSearchParams(params);
   const response = await axios.get(`adverts?${queryParams}`);
   return response.data;
-  }
-catch(Notif)
 };
